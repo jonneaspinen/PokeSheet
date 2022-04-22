a4a9f698b654b8fcb9720b463919e2962e2b197d
Jonne Aspinen <bgo957@myy.haaga-helia.com>
2022-04-22 14:17:28 +0300

    Base for the app

---
+++ screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import { React } from 'react';
+import { View, Text } from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+
+    return ( 
+        <View>
+            <Text>This is HOME screen</Text>
+        </View>
+    )
+}
+
+export default HomeScreen
---
+++ screens/PokemonListScreen.js
@@ -0,0 +1,15 @@
+import { React, useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+
+const PokemonListScreen = ({ navigation }) => {
+    
+    return (
+        <View>
+            <Text>
+                This is POKEMON screen.
+            </Text>
+        </View>
+    )
+}
+
+export default PokemonListScreen;
